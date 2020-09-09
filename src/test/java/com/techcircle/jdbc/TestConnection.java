1306f3ef326b441dc5703462de34ae532c3ae73e
Irfan Yuksel <irfanyuksel@Irfans-MacBook-Pro.local>
2020-09-09 17:44:51 -0400

    Adding jdbc code

---
+++ src/test/java/com/techcircle/jdbc/TestConnection.java
@@ -0,0 +1,30 @@
+package com.techcircle.jdbc;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestConnection {
+
+    @Test
+    public void connectionTest(){
+        System.out.println("Testing connection");
+        String connectionStr = "jdbc:oracle:thin:@18.208.119.28:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        // GETTING DATABASE CONNECTION TO THE SERVER
+        try {
+            Connection conn = DriverManager.getConnection(connectionStr,username,password);
+            System.out.println("CONNECTION Successful!!");
+            conn.close();
+        } catch (SQLException throwables) {
+            System.err.println("CONNECTION WAS NOT SUCCESSFUL \n" + throwables.getMessage());
+        }
+
+
+    }
+
+}
---
+++ src/test/java/com/techcircle/jdbc/day4/MYSQL_TEST.java
@@ -0,0 +1,25 @@
+package com.techcircle.jdbc.day4;
+
+import com.techcircle.jdbc.utility.ConfigurationReader;
+import com.techcircle.jdbc.utility.DB_Utility;
+
+public class MYSQL_TEST {
+
+    public static void main(String[] args) {
+
+        // added mysql pom dependency for mysql driver
+        // we provided connection string , username password
+        // and used the utility to make connection
+
+        String connectionStr = ConfigurationReader.getProperty("library1.database.url");
+        String username = ConfigurationReader.getProperty("library1.database.username");
+        String password = ConfigurationReader.getProperty("library1.database.password");
+
+        DB_Utility.createConnection(connectionStr,username,password);
+        DB_Utility.destroy();
+
+
+    }
+
+
+}
